8fd38c503593ce3cdcb12086339ec72f1164dbb4
pedrojjsantos <pedrojjsantos.cc@gmail.com>
2023-05-15 14:48:04 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
+/** @title Input with a custom ErrorStateMatcher */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projetoDoacao';
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+
+  matcher = new MyErrorStateMatcher();
+
+  forms!: FormGroup;
+
+  onSubmit(): void {
+  }
+}
+
+//imputs
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatCardModule} from '@angular/material/card'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { ContentHeaderComponent } from './content-header/content-header.component';
+import { MatButtonModule } from '@angular/material/button'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    ContentHeaderComponent, 
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
